10490ef249eb1bed528b356d562fcd8b59986883
Thales <tpsthales@gmail.com>
2018-12-04 18:09:32 -0200

    todos os homes

--- routes/estoque.js
+++ routes/estoque.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const {Pool} = require('pg');
+const pool = new Pool({connectionString: process.env.DATABASE_URL,
+ssl: true
+});
 
 router.get('/', function(req, res, next) {
+  pool.connect(function(err, client,done){
+  client.query('SELECT prod.idproduto,prod.descricao, SUM(estoque.quantidade) as quantidade FROM produto as prod, estoque WHERE prod.idproduto = estoque.idproduto GROUP BY prod.idproduto ASC;',function(err, result){
+  	done();
+  	if(err){
+  		console.log(err.stack);
+ 	}
   res.render('estoque', { title: 'Estoque - Genesis Laser' });
 });
 
--- routes/financeiro.js
+++ routes/financeiro.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const {Pool} = require('pg');
+const pool = new Pool({connectionString: process.env.DATABASE_URL,
+ssl: true
+});
 
 router.get('/', function(req, res, next) {
   res.render('financeiro', { title: 'Financeiro - Genesis Laser' });
