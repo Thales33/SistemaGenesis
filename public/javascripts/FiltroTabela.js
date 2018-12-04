526f95e598f06711f98750ae54c0832a5425c1ad
Thales <tpsthales@gmail.com>
2018-12-04 17:03:11 -0200

    teste layout

--- public/javascripts/FiltroTabela.js
+++ public/javascripts/FiltroTabela.js
@@ -37,3 +37,13 @@ function(document) {
 	});
 
 }(document);
+
+var $rows = $('#Tabela tr');
+$('#Pesquisa').keyup(function() {
+    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+    
+    $rows.show().filter(function() {
+        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+        return !~text.indexOf(val);
+    }).hide();
+});
