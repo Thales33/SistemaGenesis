2119629a9230208c8ab9bc67c1b30973ccc4d20a
Thales <tpsthales@gmail.com>
2018-12-04 12:43:54 -0200

    teste layout

--- routes/cliente.js
+++ routes/cliente.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const {Pool} = require('pg');
+const pool = new Pool({connectionString: process.env.DATABASE_URL,
+ssl: true
+});
 
 router.get('/', function(req, res, next) {
-  res.render('clientes/homeClientes', { title: 'Clientes - Genesis Laser' });
-});
+  pool.connect(function(err, client,done){
+  client.query('SELECT * FROM cliente order by idcliente ASC;',function(err, result){
+  	done();
+  	if(err){
+  		console.log(err.stack)
+ 	}
+
+    res.render('clientes/homeClientes', { title: 'Clientes - Genesis Laser',clientes: result });
+   });
+  });
+}); 
 
 module.exports = router; 
