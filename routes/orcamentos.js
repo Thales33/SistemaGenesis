02838598497e0aa11ef2115cd8ed030f5ca691fa
Thales <tpsthales@gmail.com>
2018-12-04 15:55:26 -0200

    teste layout

--- routes/orcamentos.js
+++ routes/orcamentos.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg');
+const {Pool} = require('pg');
+const pool = new Pool({connectionString: process.env.DATABASE_URL,
+ssl: true
+});
 
 router.get('/', function(req, res, next) {
-  res.render('orcamentos', { title: 'Orçamentos - Genesis Laser' });
-});
+	pool.connect(function(err, client,done){
+    client.query('SELECT orca.idorcamento, orca.idcliente,orca.preco, status.descricao FROM orcamentos as orca inner join cliente as cli on (orca.idcliente = cli.idcliente) inner join status on (orca.idstatus = status.idstatus) order by idorcamento ASC;',function(err, result){
+        done();
+        if(err){
+            console.log(err.stack);
+       }
+        res.render('orcamentos/homeOrcamentos', {
+        title: 'Orçamentos - Genesis Laser'
+       ,orcamentos: result
+      });
+       });
+     });
+    });
+
 
 module.exports = router;
