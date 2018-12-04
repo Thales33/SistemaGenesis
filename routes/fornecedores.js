2de57a3c411bbd37f273337d6274e7fc5f2135e0
Thales <tpsthales@gmail.com>
2018-12-04 17:29:34 -0200

    teste layout

--- routes/fornecedores.js
+++ routes/fornecedores.js
@@ -2,7 +2,41 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('fornecedores', { title: 'Fornecedores - Genesis Laser' });
+ pool.connect(function(err, client,done){
+  client.query('SELECT * FROM fornecedor order by idforncedor ASC;',function(err, result){
+  	done();
+  	if(err){
+  		console.log(err.stack);
+ 	}
+  res.render('fornecedores/homeFornecedores', { title: 'Fornecedores - Genesis Laser' });
+  });
+ });
+}); 
+
+router.get('/cadFornecedor', function(req,res){
+      res.render('fornecedores/cadFornecedor', {
+    	title: 'Cadastrar Novo Fornecedor'
+    });
+   });
+
+router.post('/addFornecedor',function(req,res){
+var nome = req.body.nome;
+var cpf = req.body.cpf;
+var telefone = req.body.telefone;
+var endereco = req.body.endereco;
+var email = req.body.email;
+var site = req.body.site;
+pool.connect(function(err, client,done){
+	client.query('INSERT INTO fornecedor (nome,cnpj,telefone,endereco,email,site) VALUES ($1,$2,$3,$4,$5,$6);',[nome,cpf,telefone,endereco,email,site,function(err, result){
+  	done();
+  	if(err){
+  		console.log(err.stack);
+  		res.send('Erro ao adicionar Fornecedor no Sistema');
+  	}else{
+     res.redirect('/fornecedor/homeFornecedores')
+  	}
+  });
+ });
 });
 
 module.exports = router;
