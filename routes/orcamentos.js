b5b355dc509526a11761c520582880997d475b35
Thales <tpsthales@gmail.com>
2018-12-04 16:10:35 -0200

    teste layout

--- routes/orcamentos.js
+++ routes/orcamentos.js
@@ -7,7 +7,7 @@ ssl: true
 
 router.get('/', function(req, res, next) {
 	pool.connect(function(err, client,done){
-    client.query('SELECT orca.idorcamento, orca.idcliente,orca.preco, status.descricao FROM orcamento as orca inner join cliente as cli on (orca.idcliente = cli.idcliente) inner join status on (orca.idstatus = status.idstatus) order by idorcamento ASC;',function(err, result){
+    client.query('SELECT orca.idorcamento, orca.idcliente,orca.preco, status.descricao,cli.nome FROM orcamento as orca inner join cliente as cli on (orca.idcliente = cli.idcliente) inner join status on (orca.idstatus = status.idstatus) order by idorcamento ASC;',function(err, result){
         done();
         if(err){
             console.log(err.stack);
@@ -19,6 +19,33 @@ router.get('/', function(req, res, next) {
        });
      });
     });
+
+router.get('/addOrcamento',function(req,res){
+        poll.connect(function(err,client, done){
+         client.query('SELECT * FROM produto', function(err,result){
+             if(err){
+                 console.log(err);
+             }
+         client.query('SELECT * FROM cliente', function(err,resultado){
+             done();
+             if(err){
+                 console.log(err);
+             }
+       client.query('SELECT * FROM status', function(err,results){
+        if(err){
+          console.log(err);
+        } 
+          res.render('orcamentos/addOrcamento',{
+              title: 'Cadastrar Novo Orçamento'
+              ,produtos: result,
+              clientes: resultado,
+              status: results
+          });    
+         });
+         });    
+        });
+     });
+    }); 
 
 
 module.exports = router;
